428295ade8353adaea378aaa39eeb92d9b36bcdc
oa23adp <160877828+oa23adp@users.noreply.github.com>
2025-03-20 17:45:41 +0000

    Updated teamwork again
    Added Ayanfe's info back (By Ayanfe)

--- baths-students/src/wars/Teamwork.java
+++ baths-students/src/wars/Teamwork.java
@@ -27,6 +27,10 @@ public class Teamwork
         details[5] = "Adebola";
         details[6] = "22086761";
 
+        details[7] = "Animashaun";
+        details[8] = "Ayanfeoluwa";
+        details[9] = "22102944";
+
 
         details[10] = "Hosseini";
         details[11] = "Pasha";
